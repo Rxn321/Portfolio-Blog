91624ed39506b763cf560c24f365bda8cf21b142
Rxn321 <79341425+Rxn321@users.noreply.github.com>
2026-08-20 17:44:33 +0800

    added new posts

--- src/data/imgOptimizer.js
+++ src/data/imgOptimizer.js
@@ -2,11 +2,16 @@ import sharp from "sharp";
 import fs from "fs";
 import path from "path";
 
-const inputDir = "./public/posts";
-const outputDir = "./public/posts/optimized";
+const inputDir = "./photos";
+const outputDir = "./public/posts/photos";
+const thumbnailDir = "./public/posts/optimized";
 
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir, { recursive: true });
+}
+
+if (!fs.existsSync(thumbnailDir)) {
+  fs.mkdirSync(thumbnailDir, { recursive: true });
 }
 
 const files = fs.readdirSync(inputDir);
@@ -14,24 +19,38 @@ const files = fs.readdirSync(inputDir);
 for (const file of files) {
   const ext = path.extname(file).toLowerCase();
 
-  if (![".jpg", ".jpeg", ".png"].includes(ext)) {
+  if (![".jpg", ".jpeg", ".png", ".heic", ".heif"].includes(ext)) {
     continue;
   }
 
   const name = path.basename(file, ext);
   const input = path.join(inputDir, file);
 
-  // Gallery thumbnail
-  await sharp(input)
-    .resize(400, 400, {
-      fit: "cover",
-    })
-    .webp({
-      quality: 50,
-    })
-    .toFile(
-      path.join(outputDir, `${name}-thumb.webp`)
-    );
-    
-  console.log(`Optimized: ${file}`);
+  try {
+    // Full WebP image
+    await sharp(input)
+      .webp({
+        quality: 100,
+      })
+      .toFile(
+        path.join(outputDir, `${name}.webp`)
+      );
+
+    // Gallery thumbnail
+    await sharp(input)
+      .resize(400, 400, {
+        fit: "cover",
+      })
+      .webp({
+        quality: 50,
+      })
+      .toFile(
+        path.join(thumbnailDir, `${name}-thumb.webp`)
+      );
+
+    console.log(`✓ Converted: ${file}`);
+  } catch (error) {
+    console.error(`✗ Failed: ${file}`);
+    console.error(error.message);
+  }
 }
--- src/data/posts.ts
+++ src/data/posts.ts
@@ -7,13 +7,88 @@ export interface Post {
   description: string;
 }
 
+// Note to self: Run imgOptimizer to optimize images and generate thumbnails before adding new posts to this
+
+
 export const posts: Post[] = [
+    {
+    id: "cypress",
+    title: "Snowboarding on Cypress Mountain",
+    date: "2025-11-23",
+    thumbnail: "/posts/optimized/cypress-thumb.webp",
+    image: "/posts/cypress.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "hollyburn",
+    title: "Hollyburn rocket on Hollyburn Mountain",
+    date: "2026-01-24",
+    thumbnail: "/posts/optimized/hollyburn-thumb.webp",
+    image: "/posts/hollyburn.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "joffreLake",
+    title: "Joffre Lake",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/joffreLake-thumb.webp",
+    image: "/posts/joffreLake.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "richmond",
+    title: "Richmond",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/richmond-thumb.webp",
+    image: "/posts/richmond.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "quebec",
+    title: "Quebec",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/quebec-thumb.webp",
+    image: "/posts/quebec.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "moab",
+    title: "Moab",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/moab-thumb.webp",
+    image: "/posts/moab.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "abiquiqui",
+    title: "Abiquiu Lake",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/abiquiqui-thumb.webp",
+    image: "/posts/abiquiqui.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
   {
-    id: "test-1",
+    id: "irec1",
     title: "IREC",
     date: "2026-08-09",
     thumbnail: "/posts/optimized/irec1-thumb.webp",
     image: "/posts/irec1.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "irec2",
+    title: "IREC",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/irec2-thumb.webp",
+    image: "/posts/irec2.jpg",
+    description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
+  },
+    {
+    id: "aso",
+    title: "IREC",
+    date: "2026-08-09",
+    thumbnail: "/posts/optimized/aso-thumb.webp",
+    image: "/posts/aso.jpg",
     description: "kajdjkawbdjkawdjkb kjsdakjhdkahw",
   },
 ]
